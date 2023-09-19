26a65e761aac30e14f53c3d7b34dd4af1a5031ea
Syed <rh_syed@outlook.com>
2023-09-19 14:43:58 -0600

    Add Open Now Icon To Card

--- src/utils/restaurant-card-styles.js
+++ src/utils/restaurant-card-styles.js
@@ -2,9 +2,11 @@ import React from "react";
 import { View, StyleSheet, Text } from "react-native";
 import styled from "styled-components";
 import { Card } from "react-native-paper";
+import { SvgXml } from "react-native-svg";
 
 export const Rating = styled(View)`
   flex-direction: row;
+  flex: 0.95;
   padding-top: ${(props) => props.theme.space[2]};
   padding-bottom: ${(props) => props.theme.space[2]};
 `;
@@ -26,6 +28,13 @@ export const RestaurantCard = styled(Card)`
   background-color: ${(props) => props.theme.colors.ui.quaternary};
 `;
 
+export const OpenXML = styled(SvgXml)`
+  flex: 0.05;
+`;
+
+export const RatingAndOpenView = styled(View)`
+  flex-direction: row;
+`;
 export const RestaurantCardCover = styled(Card.Cover)`
   padding: ${(props) => props.theme.space[3]};
   background-color: ${(props) => props.theme.colors.ui.quaternary};
