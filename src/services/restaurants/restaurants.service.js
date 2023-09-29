8af4251047c7327d72c163c610f13415c1a36eb9
Syed <rh_syed@outlook.com>
2023-09-28 18:57:31 -0600

    Transform API Response

--- src/services/restaurants/restaurants.service.js
+++ src/services/restaurants/restaurants.service.js
@@ -1,4 +1,5 @@
 import { mocks } from "../mock/index";
+import camelize from "camelize";
 
 export const restaurantsRequest = (location = "43.653225,-79.383186") => {
   return new Promise((resolve, reject) => {
@@ -9,9 +10,22 @@ export const restaurantsRequest = (location = "43.653225,-79.383186") => {
     resolve(mock);
   });
 };
+
+const restaurantTransform = ({ results = [] }) => {
+  const mappedResults = results.map((restaurant) => {
+    return {
+      ...restaurant,
+      isOpenNow: restaurant.opening_hours && restaurant.opening_hours.open_now,
+      isClosedTemporarily: restaurant.business_status === "CLOSED_TEMPORARILY",
+    };
+  });
+
+  return camelize(mappedResults);
+};
 restaurantsRequest()
-  .then((result) => {
-    console.log(result);
+  .then(restaurantTransform)
+  .then((transformedResponse) => {
+    console.log(transformedResponse);
   })
   .catch((error) => {
     console.log(error);
