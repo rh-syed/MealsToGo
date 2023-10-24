6bb77278b7be3f08aac64be71d2d467a3419ab87
Syed <rh_syed@outlook.com>
2023-10-24 16:11:30 -0600

    FireBase SetUp And Installation

--- App.js
+++ App.js
@@ -14,6 +14,21 @@ import { LocationContextProvider } from "./src/services/location/location.contex
 import { Navigation } from "./src/infrastrucutre/navigation";
 import { FavoritesContextProvider } from "./src/services/favorites/favorite.context";
 
+import { initializeApp } from "firebase/app";
+import { AuthenticationContextProvider } from "./src/services/authentication/authentication.context";
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAzz-Z_caeruyVqO0jtDb_sv0bdQMEsK78",
+  authDomain: "mealstogo-798f9.firebaseapp.com",
+  projectId: "mealstogo-798f9",
+  storageBucket: "mealstogo-798f9.appspot.com",
+  messagingSenderId: "425025172023",
+  appId: "1:425025172023:web:19df288c2b138aa1b845b2",
+};
+
+initializeApp(firebaseConfig);
+
 export default function App() {
   const [oswaldLoaded] = useOswald({
     Oswald_400Regular,
@@ -29,13 +44,15 @@ export default function App() {
   return (
     <>
       <ThemeProvider theme={theme}>
-        <FavoritesContextProvider>
-          <LocationContextProvider>
-            <RestaurantContextProvider>
-              <Navigation />
-            </RestaurantContextProvider>
-          </LocationContextProvider>
-        </FavoritesContextProvider>
+        <AuthenticationContextProvider>
+          <FavoritesContextProvider>
+            <LocationContextProvider>
+              <RestaurantContextProvider>
+                <Navigation />
+              </RestaurantContextProvider>
+            </LocationContextProvider>
+          </FavoritesContextProvider>
+        </AuthenticationContextProvider>
       </ThemeProvider>
 
       <ExpoStatusBar style="auto" />
