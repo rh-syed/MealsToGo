0f4090ab950ea82997d08969ca074daf1a53a70f
Syed <rh_syed@outlook.com>
2023-10-25 17:24:59 -0600

    Account Bg Overlay

--- src/features/account/components/account.style.js
+++ src/features/account/components/account.style.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { ImageBackground } from "react-native";
+import { View } from "react-native";
 import styled from "styled-components";
 
 export const AccountBackgroundImage = styled(ImageBackground).attrs({
@@ -8,4 +9,11 @@ export const AccountBackgroundImage = styled(ImageBackground).attrs({
   flex: 1;
   align-items: "center";
   justify-content: "center";
+`;
+
+export const AccountCoverOverlay = styled(View)`
+  position: absolute;
+  width: 100%;
+  height: 100%;
+  background-color: rgba(255, 255, 255, 0.3);
 `;
