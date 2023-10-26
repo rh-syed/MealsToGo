f47fb4c154a694f5a4ae1d5fa05abd6849b9758d
Syed <rh_syed@outlook.com>
2023-10-25 18:03:19 -0600

    Login Buttons

--- src/features/account/components/account.style.js
+++ src/features/account/components/account.style.js
@@ -2,13 +2,14 @@ import React from "react";
 import { ImageBackground } from "react-native";
 import { View } from "react-native";
 import styled from "styled-components";
-
+import { Button } from "react-native-paper";
+import { colors } from "../../../infrastrucutre/theme/colors";
 export const AccountBackgroundImage = styled(ImageBackground).attrs({
   source: require("../../../../assets/home_bg.jpeg"),
 })`
   flex: 1;
-  align-items: "center";
-  justify-content: "center";
+  align-items: center;
+  justify-content: center;
 `;
 
 export const AccountCoverOverlay = styled(View)`
@@ -16,4 +17,16 @@ export const AccountCoverOverlay = styled(View)`
   width: 100%;
   height: 100%;
   background-color: rgba(255, 255, 255, 0.3);
+`;
+
+export const AccountButtonsContainer = styled(View)`
+  background-color: rgba(255, 255, 255, 0.7);
+  padding: ${(props) => props.theme.space[4]};
+  margin-top: ${(props) => props.theme.space[2]};
+`;
+
+export const AuthButton = styled(Button).attrs({
+  color: colors.brand.primary,
+})`
+  padding: ${(props) => props.theme.space[2]};
 `;
--- src/features/account/screens/account.screen.js
+++ src/features/account/screens/account.screen.js
@@ -1,13 +1,41 @@
 import React from "react";
 import {
   AccountBackgroundImage,
+  AccountButtonsContainer,
   AccountCoverOverlay,
+  AuthButton,
 } from "../components/account.style";
+import {
+  NAV_KEY_LOGIN_SCREEN,
+  NAV_KEY_REGISTER_SCREEN,
+} from "../../../infrastrucutre/navigation/utils/navigation-keys";
+import { Spacer } from "../../../components/spacer/spacer.component";
 
-export const AccountScreen = () => {
+export const AccountScreen = ({ navigation }) => {
   return (
     <AccountBackgroundImage>
       <AccountCoverOverlay />
+      <AccountButtonsContainer>
+        <Spacer size="large">
+          <AuthButton
+            icon="lock-open-outline"
+            mode="contained"
+            onPress={() => navigation.navigate(NAV_KEY_LOGIN_SCREEN)}
+          >
+            Login
+          </AuthButton>
+        </Spacer>
+
+        <Spacer size="large">
+          <AuthButton
+            icon="location-enter"
+            mode="contained"
+            onPress={() => navigation.navigate(NAV_KEY_REGISTER_SCREEN)}
+          >
+            Register
+          </AuthButton>
+        </Spacer>
+      </AccountButtonsContainer>
     </AccountBackgroundImage>
   );
 };
